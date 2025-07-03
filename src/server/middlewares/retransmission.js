f6fde8126e9adc54572981a8320441ecf93b1ae7
lixiaoshuang <lixiaoshuang@corp.netease.com>
2025-07-03 15:27:15 +0800

    feat: 🎸 socket support file transmit

--- src/server/middlewares/retransmission.js
+++ src/server/middlewares/retransmission.js
@@ -1,14 +1,53 @@
+import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 
 import { recoder, CONNECTION } from '../../shared/index.js';
 
-const keys = [
-  'body',
-  'type',
-  'href',
-  'method',
-  'headers',
-];
+const getFromRequest = (key) => (ctx) => ctx?.request?.[key];
+
+const fileToBuffer = (file = {}) => {
+  const { filepath } = file;
+
+  return filepath && fs.readFileSync(filepath);
+};
+
+const forEntries = (source = {}) => (callback, object = {}) => {
+  if (!callback) {
+    return { ...object, ...source };
+  }
+
+  const entries = Object.entries(source);
+  const reduce = (result = {}, entry = []) => {
+    const [key, value] = entry;
+    const got = callback(value);
+
+    return { ...result, [key]: got };
+  };
+
+  return entries.reduce(reduce, object);
+};
+
+const getFilesFromRequest = (ctx = {}) => {
+  const { request: { files } = {} } = ctx;
+
+  const callback = (file = {}) => {
+    const source = file.toJSON();
+    const buffer = fileToBuffer(file);
+
+    return { ...source, buffer };
+  };
+
+  return files && forEntries(files)(callback);
+};
+
+const bundle = {
+  files: getFilesFromRequest,
+  body: getFromRequest('body'),
+  type: getFromRequest('type'),
+  href: getFromRequest('href'),
+  method: getFromRequest('method'),
+  headers: getFromRequest('headers'),
+};
 
 const promise = (beacon) => {
   const handler = (resolve, reject) => {
@@ -18,6 +57,10 @@ const promise = (beacon) => {
   };
 
   return new Promise(handler);
+};
+
+const promiseToContext = async (ctx, beacon) => {
+  ctx.body = await promise(beacon);
 };
 
 const retransmission = (ctx = {}) => {
@@ -31,15 +74,11 @@ const retransmission = (ctx = {}) => {
   }
 
   const beacon = uuidv4();
-  const reduce = (result = {}, key) => {
-    result[key] = request[key];
-    return result;
-  };
-
-  const combined = keys.reduce(reduce, { beacon });
+  const callback = (handler) => handler(ctx);
+  const combined = forEntries(bundle)(callback, { beacon });
 
   socket.emit(CONNECTION.REQUEST, combined);
-  return promise(beacon);
+  return promiseToContext(ctx, beacon);
 };
 
 export default retransmission;
